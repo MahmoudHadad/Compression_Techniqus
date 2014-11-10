c19fba49841c3584ebacafd3bc971b3a33257053
hooda hadad <hooda_fci@yahoo.com>
2014-11-10 22:05:50 +0200

    quick sort method created and tested

--- src/src/com/yahoo/hooda_fci/hufmmanTechniques/Main.java
+++ src/src/com/yahoo/hooda_fci/hufmmanTechniques/Main.java
@@ -1,9 +1,25 @@
 package com.yahoo.hooda_fci.hufmmanTechniques;
 
+import java.util.ArrayList;
+
+
 public class Main {
 	
 	public static void main (String [] args)
 	{
+		
+		//System.out.println(Integer.toBinaryString( (int)'a' ) );
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		a.add(9);
+		a.add(5);
+		a.add(7);
+		ArrayList<String> s = new ArrayList<String>();
+		s.add("9");
+		s.add("5");
+		s.add("7");
+		StandardHuffman.quickSort(s, a);
+		
+		System.out.println( a + "\n" + s);
 		
 		
 		
--- src/src/com/yahoo/hooda_fci/hufmmanTechniques/StandardHuffman.java
+++ src/src/com/yahoo/hooda_fci/hufmmanTechniques/StandardHuffman.java
@@ -1,8 +1,87 @@
 package com.yahoo.hooda_fci.hufmmanTechniques;
 
+import java.util.ArrayList;
+
 public class StandardHuffman {
 	
+	public static String compress ( String data)
+	{
+		// to hold each character and its probability
+		ArrayList<String>chars = new ArrayList<String>();
+		ArrayList<Integer>probs = new ArrayList<Integer>();
+		
+		
+		
+		
+		return new String();
+	}
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void quickSort(ArrayList<String>chars , ArrayList<Integer>probs)
+	{
+		recQuickSort(chars, probs, 0 , probs.size()-1);
+	}
+	
+	private static void recQuickSort(ArrayList<String>chars , ArrayList<Integer>probs, int l, int r)
+	{
+		if(l>=r)return;
 
+		int i=l, j=r;
+		int piv=(l+r)/2;
+		
+		while(i<j)
+		{
+			while(probs.get(i)<probs.get(piv))
+			i++;
+
+			while(probs.get(j)>probs.get(piv))
+			j--;
+			
+			// swapping
+			
+			if(i<=j)
+			{
+				Integer iTemp = probs.get(i);
+				probs.set(i, probs.get(j) ) ;
+				probs.set(j, iTemp);
+				
+				String sTemp = chars.get(i);
+				chars.set(i, chars.get(j) ) ;
+				chars.set(j, sTemp);
+			}
+			
+			i++;
+			j--;
+			
+		}
+
+		if (l < j) 
+			recQuickSort(chars,probs , l, j); 
+		if (i < r) 
+			recQuickSort(chars,probs, i, r);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
 }
